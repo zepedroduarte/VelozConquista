d3e98ec95c0ce0d9cbdd13f63f045bcf509e8275
José Duarte <65547533+zepedroduarte@users.noreply.github.com>
2025-07-18 02:07:57 +0100

    FE Creation

---
+++ fe/src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { Header } from "@/components/app/header";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Veloz Conquista",
+  description: "Comércio de automóveis usados e semi-novos.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ fe/src/components/app/header.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { Menu, Search } from "lucide-react";
+
+
+export function Header() {
+  return (
+    <span>aaaaaaaaaaaaa</span>
+  )
+}
